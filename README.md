359f1b9cd2baf5b1ee5252f5baa68ba0ebf3d5df
Lilian <lilianforger@gmail.com>
2022-09-16 16:43:54 +0200

    Step 3 - Added our first request to the controller

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Changes:
 
-- Go to the newly created models folder, this step covers all about that!
+- Now that our setup is in place, we can go to our Author controller functions and start querying our database with the Mongoose methods.
+- We will build a simple GET request to retrieve all the authors from the database.
+- For that, you need to add one or two authors to the database manually, otherwise proceed to step 5 where we build a POST request to add authors to the database.
--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,5 +1,21 @@
-const getAuthors = async (req, res) => {
+// Since we will have no html/front end to test our API, we will rely on Postman or Insomnia to
+// test our CRUD operations and see if our backend is responding as expected.
 
+// Firstly we need to import our Author collection from our models
+const AuthorCollection = require('../models/authorModel');
+
+// Given our database queries can take a little to complete, we will use async/await to handle them
+const getAuthors = async (req, res) => {
+  // With async/await, we use try/catch to handle errors
+  try {
+    // Then we can query our database! This function is meant to return all authors in the database
+    const authors = await AuthorCollection.find();
+    // And we return to our client the query in JSON format.
+    res.status(200).json(authors);
+  } catch (error) {
+    // If there is an error, we will send a 500 status code and the error's message to the client
+    res.status(500).send(error.message)
+  }
 }
 
 const getAuthorById = async (req, res) => {
